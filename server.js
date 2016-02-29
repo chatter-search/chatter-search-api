4d9a1560f3f82b946fbcdcf501b21f6559b23d95
dmi3y <lapshukov@gmail.com>
2016-02-29 01:19:39 -0800

    proxy to user timeline

--- server.js
+++ server.js
@@ -2,9 +2,30 @@
 
 var express = require('express');
 var app = express();
+var oauth = require("./lib/oauth");
+var userTimelineApi = require("./lib/user-timeline");
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
+app.get('/proxy-user-timeline', function (req, res) {
+	var oauthPromise = oauth();
+ 	oauthPromise.then(
+ 		function(bearer) {
+
+ 			return userTimelineApi({
+ 				bearer: bearer
+ 			});
+ 		}
+ 	).then(
+ 		function(json) {
+
+ 			res.send(json);
+ 		},
+ 		function error(err) {
+
+			res.send({
+				"error": err
+			});
+		}
+ 	);
 });
 
 app.listen(process.env.PORT || 8080, process.env.IP || "0.0.0.0");
