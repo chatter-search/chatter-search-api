8e3e2f655539ab9535595abb41be834cfbfd7b31
dmi3y <lapshukov@gmail.com>
2016-03-15 23:24:09 -0700

    formatting... mostrly I hope...

--- src/oauth.js
+++ src/oauth.js
@@ -1,38 +1,34 @@
-'use strict';
+'use strict'
 
-var OAuth = require('oauth');
-var OAuth2 = OAuth.OAuth2;
-var twitterConsumerKey = process.env.OAUTH_TWITTER_CONSUMER_KEY;
-var twitterConsumerSecret = process.env.OAUTH_TWITTER_CONSUMER_SECRET;
-var twitterApiUri = process.env.TWITTER_API_URI;
+var OAuth = require('oauth')
+var OAuth2 = OAuth.OAuth2
+var twitterConsumerKey = process.env.OAUTH_TWITTER_CONSUMER_KEY
+var twitterConsumerSecret = process.env.OAUTH_TWITTER_CONSUMER_SECRET
+var twitterApiUri = process.env.TWITTER_API_URI
 
 var oauth2 = new OAuth2(
-	twitterConsumerKey,
-	twitterConsumerSecret,
-	twitterApiUri,
-	null,
-	'oauth2/token',
-	null
-);
+  twitterConsumerKey,
+  twitterConsumerSecret,
+  twitterApiUri,
+  null,
+  'oauth2/token',
+  null
+)
 
-module.exports = function() {
-
-	return new Promise(function(rs, rj) {
-
-		oauth2.getOAuthAccessToken(
-			'',
-			{
-				'grant_type': 'client_credentials'
-			},
-			function(err, access_token, refresh_token, results) {
-				if ( err ) {
-
-					rj(err);
-				} else {
-
-					rs(access_token);
-				}
-			}
-		);
-	});
-};
+module.exports = function () {
+  return new Promise(function (resolve, reject) {
+    oauth2.getOAuthAccessToken(
+      '',
+      {
+        'grant_type': 'client_credentials'
+      },
+      function (err, access_token, refresh_token, results) {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(access_token)
+        }
+      }
+    )
+  })
+}
